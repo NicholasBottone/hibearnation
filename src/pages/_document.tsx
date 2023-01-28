07d7f14c2af50cf385afe51f6b797fdc8ec8e6d6
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-01-28 11:50:56 -0500

    home page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
