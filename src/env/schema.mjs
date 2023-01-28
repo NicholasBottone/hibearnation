9af5ce864d80ca1d8bebcdd688ef8c76f373c795
Nicholas Bottone <nick@bottone.io>
2023-01-28 10:30:42 -0500

    Fix google env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -33,8 +33,8 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  GOOGLE_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
 
 /**
