6f9f0e83bbf7dba9d0bb2779bb2bdfb026536910
Nicholas Bottone <nick@bottone.io>
2023-01-28 15:07:20 -0500

    Implement locations api router
    Co-authored-by: OrenKohavi <OrenKohavi@users.noreply.github.com>

---
+++ src/server/api/routers/locations.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const locationsRouter = createTRPCRouter({
+  // Returns full data of all the locations
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.location.findMany();
+  }),
+
+  // Returns partial data of all the locations
+  getNames: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.location.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+  }),
+
+  // Returns full data of a single location
+  getOne: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.location.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
