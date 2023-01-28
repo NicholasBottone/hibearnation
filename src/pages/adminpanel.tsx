3f96cbd09b078ec7a7ecf4c4c85467f1f1fbaaf7
Oren Kohavi <Oren_Kohavi@brown.edu>
2023-01-28 17:24:43 -0500

    implement WIP admin panel

---
+++ src/pages/adminpanel.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { type NextPage } from "next";
+import { api } from "../utils/api";
+import { signIn } from "next-auth/react";
+
+const Adminpanel: NextPage = () => {
+  const createAreasMutation = api.admin.createAreas.useMutation();
+  const [areaName, setAreaName] = useState("");
+
+  const addArea = () => {
+    console.log("Adding area: ", areaName);
+    //alert("Adding area: " + areaName);
+    createAreasMutation.mutate({ name: areaName });
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className="admin-header">
+          <h1 style={{ textAlign: "center" }}>Admin Panel</h1>
+          <h3 style={{ textAlign: "center" }}>
+            Add new areas, locations, etc.
+          </h3>
+        </div>
+
+        <button
+          onClick={() => {
+            console.log("Logging In...");
+            signIn();
+          }}
+        >
+          Login
+        </button>
+
+        <div
+          className="add-area"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h2 style={{ textAlign: "center" }}>Add Area:</h2>
+          <label htmlFor="Area Name">Area Name:</label>
+          <input
+            id="Area Name"
+            type="text"
+            value={areaName}
+            onChange={(e) => setAreaName(e.target.value)}
+          />
+
+          <button onClick={addArea}>Add Area</button>
+        </div>
+
+        <div className="add-location">
+          <h2 style={{ textAlign: "center" }}>Add Location:</h2>
+          <input id="Location Name" type="text" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Adminpanel;
--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,45 +1,56 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
 
 export const adminRouter = createTRPCRouter({
   // Creates new areas
-  createAreas: protectedProcedure.mutation(async ({ ctx }) => {
-    if (!ctx.session.user.admin) {
-      return "You are not an admin";
-    }
+  createAreas: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.session.user.admin) {
+        return "You are not an admin";
+      }
 
-    await ctx.prisma.area.createMany({
-      data: [
-        {
-          name: "New Area",
+      await ctx.prisma.area.create({
+        data: {
+          name: input.name,
         },
-      ],
-    });
+      });
 
-    return "Areas created";
-  }),
+      return "Areas created";
+    }),
 
   // Creates new locations
-  createLocation: protectedProcedure.mutation(async ({ ctx }) => {
-    if (!ctx.session.user.admin) {
-      return "You are not an admin";
-    }
+  createLocation: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        summary: z.string(),
+        areaName: z.string(),
+        address: z.string(),
+        coordinates: z.string(),
+        sublocations: z.array(z.string()),
+        floorplans: z.array(z.string()),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.session.user.admin) {
+        return "You are not an admin";
+      }
 
-    await ctx.prisma.location.createMany({
-      data: [
-        {
-          name: "New Location",
-          summary: "New Location Summary",
-          areaName: "New Area",
-          address: "1234 Main St",
-          coordinates: "1234, 5678",
-          sublocations: ["1234", "5678"],
-          floorplans: ["abcd", "abcd"],
+      await ctx.prisma.location.create({
+        data: {
+          name: input.name,
+          summary: input.summary,
+          areaName: input.areaName,
+          address: input.address,
+          coordinates: input.coordinates,
+          sublocations: input.sublocations,
+          floorplans: input.floorplans,
         },
-      ],
-    });
+      });
 
-    return "Locations created";
-  }),
+      return "Locations created";
+    }),
 
   //
 });
