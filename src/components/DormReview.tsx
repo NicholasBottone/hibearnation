51e0d0273daceaed3c8f9b083834af3a2a3a686f
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-01-29 01:45:31 -0500

    badges

--- src/components/DormReview.tsx
+++ src/components/DormReview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./DormReview.module.css";
 import { BiUpvote, BiDownvote } from "react-icons/bi";
 import { ImArrowUp, ImArrowDown } from "react-icons/im";
+import { useState } from "react";
 
 interface BuildingReviewProps {
   id: number;
@@ -23,23 +24,63 @@ interface BuildingReviewProps {
 }
 
 export default function BuildingReview(props: BuildingReviewProps) {
+  const upvoteCount = props.upvotes ? props.upvotes.length : 0;
+  const downvoteCount = props.downvotes ? props.downvotes.length : 0;
+  const [count, setCount] = useState(upvoteCount - downvoteCount);
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
+
   return (
     <div className={styles.BuildingReview}>
-      <div className={styles.UpvoteDownvote}>
-        <div className={styles.Pair}>
-          <p>{props.upvotes ? props.upvotes.length : 0}</p>
+      <div className={styles.UpvoteDownvoteAndNumber}>
+        {count}
+        <div className={styles.UpvoteDownvote}>
           <BiUpvote className={styles.Upvote} />
-        </div>
-        <div className={styles.Pair}>
-          <p>{props.downvotes ? props.downvotes.length : 0}</p>
           <BiDownvote className={styles.Downvote} />
         </div>
       </div>
       <div className={styles.reviewSection}>
         <div className={styles.reviewHeader}>
-          <strong>
-            {props.author} - {props.overallRating / 2} / 5
-          </strong>
+          <div
+            style={{
+              backgroundColor: "#fce9a2",
+              padding: "0.03rem 0.4rem",
+              borderRadius: "0.2rem",
+              border: "1px solid #f9d56e",
+            }}
+          >
+            {props.overallRating / 2} / 5 <b className={styles.emoji}>⭐</b>
+          </div>
+          <div
+            style={{
+              backgroundColor: "#a2f0f9",
+              padding: "0.03rem 0.4rem",
+              borderRadius: "0.2rem",
+              border: "1px solid #6ed5f9",
+            }}
+          >
+            {props.amenitiesRating / 2} / 5 <b className={styles.emoji}>🚿</b>
+          </div>
+          <div
+            style={{
+              backgroundColor: "#f9a2a2",
+              padding: "0.03rem 0.4rem",
+              borderRadius: "0.2rem",
+              border: "1px solid #f96e6e",
+            }}
+          >
+            {props.comfortRating / 2} / 5 <b className={styles.emoji}>🛌</b>
+          </div>
+          <div
+            style={{
+              backgroundColor: "#a2f0a2",
+              padding: "0.03rem 0.4rem",
+              borderRadius: "0.2rem",
+              border: "1px solid #6ef96e",
+            }}
+          >
+            {props.locationRating / 2} / 5 <b className={styles.emoji}>📍</b>
+          </div>
         </div>
         <div className="reviewContent">{props.body}</div>
       </div>
