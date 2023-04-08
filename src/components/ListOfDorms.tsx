1c1819476700d0f41892be0643952b247e3f2b55
nicholasvadasz <nicholas_vadasz@brown.edu>
2023-04-08 04:28:12 -0400

    better mobile dorm list

--- src/components/ListOfDorms.tsx
+++ src/components/ListOfDorms.tsx
@@ -13,6 +13,9 @@ interface ListOfDormsProps {
 export default function ListOfDorms(props: ListOfDormsProps) {
   const router = useRouter();
   const getOS = () => {
+    if (typeof window === "undefined") {
+      return null;
+    }
     const userAgent = window.navigator.userAgent;
     const platform = window.navigator.platform;
     const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
